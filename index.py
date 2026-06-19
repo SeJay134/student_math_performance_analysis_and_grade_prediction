fd3531894742f5384b1c9e43cd0fdc9b7325b4f8
SeJay134 <patrushev.s.job@gmail.com>
2026-06-19 16:32:43 -0400

    histogram

--- index.py
+++ index.py
@@ -22,3 +22,26 @@ print('df.head(5)\n', df.head(5))
 print('df.dtypes\n', df.dtypes)
 print('df.isna().sum()\n', df.isna().sum())
 print('df.duplicated().sum()\n', df.duplicated().sum())
+
+counts, bins = np.histogram(df['G3'], bins=[x-0.5 for x in range(0,22)])
+
+plt.figure(figsize=(7, 5))
+plt.hist(df['G3'], bins=[x - 0.5 for x in range(0, 22)], edgecolor='black', linewidth=0.5)
+
+for count, bin_start in zip(counts, bins[:-1]):
+    plt.text(
+        x=bin_start + 0.5,
+        y=count + 0.2,
+        s=int(count),
+        ha='center',
+        va='bottom',
+        fontsize=9
+    )
+
+plt.title('Distribution of Final Math Grades')
+plt.xticks(range(0, 21, 1))
+plt.xlabel('Final Grade (G3)')
+plt.ylabel('Number of Students')
+plt.savefig('outputs/g3_distribution.png')
+plt.show()
+
