6ba41bc08aed1f6b94bd5adc34b883dd7aab25c0
SeJay134 <patrushev.s.job@gmail.com>
2026-06-19 16:31:21 -0400

    read csv

--- index.py
+++ index.py
@@ -6,3 +6,19 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 
+os.makedirs('outputs', exist_ok=True)
+
+try:
+    df = pd.read_csv('student_performance_math.csv', sep=';')
+except Exception as e:
+    print(f'error {e}')
+
+print('df.info')
+df.info()
+
+print('df.shape\n', df.shape)
+print('df.head(5)\n', df.head(5))
+
+print('df.dtypes\n', df.dtypes)
+print('df.isna().sum()\n', df.isna().sum())
+print('df.duplicated().sum()\n', df.duplicated().sum())
